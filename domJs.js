fb83f1e112a8c6c63643d3e6beee97245997d849
Taylor Mastin <thmastin@gmail.com>
2022-10-25 22:52:29 -0400

    Finshe Dom Exercises

--- domJs.js
+++ domJs.js
@@ -41,4 +41,6 @@ newDiv.appendChild(newPara);
 container.appendChild(newDiv);
 
 const btn = document.querySelector('#btn');
-btn.onclick = () => alert("Hello World");
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+})
